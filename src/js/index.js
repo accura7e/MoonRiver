3f484e2abea49e49433e3b1b37dab7868d5aca8e
accura7e <accura7ee@gmail.com>
2024-04-18 23:25:38 +0300

    add: added slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+import slider from './modules/slider.js';
+slider()
---
+++ src/js/modules/slider.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+
+function slider(){
+    // import Swiper JS
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        slidesPerView: 4,
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}
+
+export default slider;
